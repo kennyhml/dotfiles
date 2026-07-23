b1ec749642edb3c52a919745ab90fbe2451ca1c2
kennyhml <kendrick.hommel@gmail.com>
2026-07-23 21:17:36 +0200

    fix: cleaup blink config

--- nvim/.config/nvim/lua/kennyhml/lazy/lsp.lua
+++ nvim/.config/nvim/lua/kennyhml/lazy/lsp.lua
@@ -2,9 +2,6 @@ return {
   -- Main LSP Configuration
   'neovim/nvim-lspconfig',
   dependencies = {
-    -- Automatically install LSPs and related tools to stdpath for Neovim
-    -- Mason must be loaded before its dependents so we need to set it up here.
-    -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
     {
       'mason-org/mason.nvim',
       ---@module 'mason.settings'
@@ -15,9 +12,8 @@ return {
     -- Maps LSP server names between nvim-lspconfig and Mason package names.
     'mason-org/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-    -- Useful status updates for LSP.
     { 'j-hui/fidget.nvim', opts = {} },
+    { 'saghen/blink.cmp' },
   },
   config = function()
     --  This function gets run when an LSP attaches to a particular buffer.
@@ -114,6 +110,7 @@ return {
 
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
     for name, server in pairs(servers) do
+      server.capabilities = require('blink.cmp').get_lsp_capabilities(server.capabilities, true)
       vim.lsp.config(name, server)
       vim.lsp.enable(name)
     end
